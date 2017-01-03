54ea9817528f96436bfa3f2fbff169ac2495e6e1
Cheolwon <lcw@lcw.local>
2017-01-04 00:29:07 +0900

    Spring boot - gradle init

---
+++ Templator/src/main/java/com/templater/TemplatorApplication.java
@@ -0,0 +1,12 @@
+package com.templater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatorApplication.class, args);
+	}
+}
