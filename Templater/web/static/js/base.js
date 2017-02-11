06928f494fb4e9a25b7ee6c17989f155111c07a4
Hansol-Lim <molmoty@gmail.com>
2017-02-11 20:35:34 +0900

    헤더 이벤트 추가

--- Templater/web/static/js/base.js
+++ Templater/web/static/js/base.js
@@ -1,12 +1,26 @@
-/**
- * Created by molmo on 2017-02-06.
- */
 $(function () {
-    $(document).on("click", "#js_header_profile", function () {
-        if(!modules.profile) {
-            $.get("../../templates/header/profile.mustache", function (profile) {
-                modules.profile = $(profile);
-            });
-        }
+    var _ = new Templater();
+
+    $("img").on("error", function () {
+        var defaultSrc = $(this).data("default");
+        if (defaultSrc) $(this).attr("src", defaultSrc);
+        else $(this).remove();
     });
+
+    $("#js_header_profile").on("click", function () {
+        var offset = $(this).offset();
+        var height = $(this).height();
+        var x = offset.left, y = offset.top;
+
+        _.load.module({
+            key: this.id,
+            url: "./templates/header/profile.mustache",
+            callback: function () {
+                _.print.module_only(this.key, {
+                    name: "Solecita",
+                    email: "aboveall7035@gmail.com"
+                })(x + 15,y + height + 15).addClass("pinned");
+            }
+        })
+    })
 });
