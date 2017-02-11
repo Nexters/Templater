0745c06f8ef0ed65b4f0f0c59ef34386b34b21ef
taeunChoi <techoi0829@gmail.com>
2017-02-11 23:58:59 +0900

    add user api

--- Templater/src/main/java/com/templater/user/security/SercurityConfig.java
+++ Templater/src/main/java/com/templater/user/security/SercurityConfig.java
@@ -2,43 +2,71 @@ package com.templater.user.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
+
+import com.templater.user.repository.UserRepository;
 import com.templater.user.service.UserServiceImpl;
 
+@Configuration
+@EnableWebSecurity
 public class SercurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired UserRepository userRepository;
+	//@Autowired CORSFilter corsFilter;
 	@Autowired
 	UserServiceImpl userServiceImpl;
+//	@Autowired
+//	UserServiceCustomImpl userServiceCustomImpl;
 
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
     	http
         	.csrf().disable()
-        	.authorizeRequests()
-            	.antMatchers("/user/login").permitAll()
-//            	.antMatchers("/user").hasAuthority("USER")
-//            	.antMatchers("/admin").hasAuthority("ADMIN")
-            	.anyRequest().authenticated()
+        		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
             .and()
-//        .formLogin()
-//             .and()
-        .logout();            
+        		.authorizeRequests()
+        			.antMatchers("/**").permitAll()
+//            		.antMatchers("/user/login").permitAll()
+//            		.antMatchers("/**").hasAuthority("USER")
+//            		.antMatchers("/admin").hasAuthority("ADMIN")
+            .and()
+            	.logout()
+            .and()
+            	.httpBasic();
+    	
     }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//         auth.userDetailsService(userService)
-//              .passwordEncoder(userService.passwordEncoder());
-    }
-    
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-         return super.authenticationManagerBean();
-    }
+	
+//    @Bean(name=BeanIds.AUTHENTICATION_MANAGER)
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//    	System.out.println("dddd");
+//        return super.authenticationManagerBean();
+//    }
 
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	};
+
+	@Bean
+	public SecurityContextRepository securityContextRepository() {
+		return new HttpSessionSecurityContextRepository();
+	}
+//	 @Bean
+//     public HttpSessionStrategy httpSessionStrategy() {
+//               return new HeaderHttpSessionStrategy();
+//     }
 
 }
