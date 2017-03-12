26987e8e0b0e985808b7bc83b99f40b39a13d767
Hansol-Lim <molmoty@gmail.com>
2017-03-12 23:36:03 +0900

    중간점검

--- static/js/base.js
+++ static/js/base.js
@@ -1,78 +1,38 @@
 $(function () {
-    var QueryString = function () {
-        // This function is anonymous, is executed immediately and
-        // the return value is assigned to QueryString!
-        var query_string = {};
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
-            // If first entry with this name
-            if (typeof query_string[pair[0]] === "undefined") {
-                query_string[pair[0]] = decodeURIComponent(pair[1]);
-                // If second entry with this name
-            } else if (typeof query_string[pair[0]] === "string") {
-                var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-                query_string[pair[0]] = arr;
-                // If third or later entry with this name
-            } else {
-                query_string[pair[0]].push(decodeURIComponent(pair[1]));
-            }
-        }
-        return query_string;
-    }();
+  window._ = new Templater();
+  _.router = new Router(_);
 
-    window._ = new Templater();
-    var route = new Router(_);
+  $("img").on("error", function () {
+    var defaultSrc = $(this).data("default");
+    if (defaultSrc) $(this).attr("src", defaultSrc);
+    else $(this).remove();
+  });
 
-    $("img").on("error", function () {
-        var defaultSrc = $(this).data("default");
-        if (defaultSrc) $(this).attr("src", defaultSrc);
-        else $(this).remove();
-    });
+  $("#js_header_profile").on("click", function () {
+    var offset = $(this).offset();
+    var height = $(this).height();
+    var x = offset.left + 15;
+    var y = offset.top + 15 + height;
+    var key = this.id;
 
-    $("#js_header_profile").on("click", function () {
-        var offset = $(this).offset();
-        var height = $(this).height();
-        var x = offset.left + 15;
-        var y = offset.top + 15 + height;
-        var key = this.id;
+    _.load.module({
+      key: key,
+      url: "./templates/header/profile.mustache",
+      callback: function () {
+        _.print.module_only(this.key, {
+          name: "Solecita",
+          email: "aboveall7035@gmail.com"
+        })(x, y, 10).addClass("pinned");
 
-        _.load.module({
-            key: key,
-            url: "./templates/header/profile.mustache",
-            callback: function () {
-                _.print.module_only(this.key, {
-                    name: "Solecita",
-                    email: "aboveall7035@gmail.com"
-                })(x, y, 10).addClass("pinned");
-
-                $(document).on('click', function (e) {
-                    if ($(e.target).parents(".module[data-id=" + key + "]").length <= 0) {
-                        $(".module[data-id=" + key + "]").remove();
-                    }
-                });
-            }
+        $(document).on('click', function (e) {
+          if ($(e.target).parents(".module[data-id=" + key + "]").length <= 0) {
+            $(".module[data-id=" + key + "]").remove();
+          }
         });
+      }
     });
+  });
 
-    switch(QueryString['page']) {
-        case "editor":
-            route.editor({
-                nav: {
-                    title: "Untitled Documents"
-                }
-            });
-            break;
-        case "list":
-            route.list();
-            break;
-        case "signup":
-            route.signup();
-            break;
-        case "login":
-            route.login();
-            break;
-    }
-
+  var params = _.get.params();
+  _.router.do(params['spa']);
 });
--- static/js/router.js
+++ static/js/router.js
@@ -1,183 +1,229 @@
 window.Router = function (_, init) {
-    init = init || true;
+  init = init || true;
 
-    if (!(_ && typeof Templater === "function")) {
-        throw Error("템플레터 라이브러리를 먼저 로드해주세요.");
+  if (!(_ && typeof Templater === "function")) {
+    throw Error("템플레터 라이브러리를 먼저 로드해주세요.");
+  }
+
+  var header = {
+    self: $("header#header"),
+    _search: true,
+    search: function (exist) {
+      this._search = exist;
+      if (this._search) this.self.removeClass("no-search");
+      else this.self.addClass("no-search");
     }
+  };
 
-    var header = {
-        self: $("header#header"),
-        _search: true,
-        search: function (exist) {
-            this._search = exist;
-            if (this._search) this.self.removeClass("no-search");
-            else this.self.addClass("no-search");
+  var panic = function () {
+    header.search(false);
+  };
+
+  if (init) {
+    panic();
+  }
+  var host = $(location).attr('protocol') + '//' + $(location).attr('host');
+  host = 'http://localhost:63342/Templater/Templater/web/';
+
+  var router = {
+    _table: function (url) {
+      var routeTable = {
+        '/': {
+          title: '초기화면',
+          action: this.editor
+        },
+        '/signup': {
+          title: '회원가입',
+          action: this.signup
+        },
+        '/login': {
+          title: '로그인',
+          action: this.login
+        },
+        '/list': {
+          title: '리스트',
+          action: this.list
+        },
+        '/edit': {
+          title: '에디터',
+          action: this.editor
         }
-    };
+      };
+      return routeTable[url];
+    },
+    do: function (url, data) {
+      var currentPath = $(location).attr('href').replace(host, '').split('?')[0];
+      url = (url || '/').replace(host, '');
+      var route = this._table(url);
 
-    var panic = function () {
-        header.search(false);
-    };
+      _.print.empty();
 
-    if (init) {
-        panic();
+      //history.pushState({back: currentPath}, route.title, host + url);
+      history.pushState({back: currentPath}, route.title, host + '?spa=' + url);
+      if (route.action) {
+        route.action.apply(null, [data]);
+      }
+    },
+    list: function (data) {
+      data = data || {};
+      switch (data.tab) {
+        default:
+        case "document":
+          data.body = $.extend(data.body || {}, {
+            tab_form: "display:none;"
+          });
+          break;
+        case "form":
+          data.body = $.extend(data.body || {}, {
+            tab_document: "display:none;"
+          });
+          break;
+      }
+
+      var documentLength = data.body.documents ? data.body.documents.length : 0;
+      var formLength = data.body.forms ? data.body.forms.length : 0;
+      if (documentLength < 8) {
+        data.body.empty_document = new Array(8 - documentLength);
+      }
+      if (formLength < 8) {
+        data.body.empty_form = new Array(8 - formLength);
+      }
+      _.load.module({
+        key: 'list-nav',
+        url: "./templates/list/nav.mustache",
+        callback: function () {
+          _.print.module_only(
+            'list-nav',
+            data.nav,
+            "nav#nav"
+          )(0, 0);
+        }
+      });
+      _.load.module({
+        key: 'list-body',
+        url: "./templates/list/index.mustache",
+        callback: function () {
+          _.print.module_only(
+            'list-body',
+            data.body,
+            "article#article"
+          )(0, 0);
+        }
+      });
     }
+    ,
+    editor: function (data) {
+      data = data || {};
+      var nav = function () {
+        var dfd = $.Deferred();
+        _.load.module({
+          key: 'editor-nav',
+          url: "./templates/editor/nav.mustache",
+          callback: function () {
+            _.print.module_only(
+              'editor-nav',
+              data.nav,
+              "nav#nav"
+            )(0, 0, 1);
+            dfd.resolve();
+          }
+        });
+        return dfd;
+      };
 
-    return {
-        list: function (data) {
-            data = data || {};
-            switch (data.tab) {
-                default:
-                case "document":
-                    data.body = $.extend(data.body || {}, {
-                        tab_form: "display:none;"
-                    });
-                    break;
-                case "form":
-                    data.body = $.extend(data.body || {}, {
-                        tab_document: "display:none;"
-                    });
-                    break;
-            }
+      var body = function () {
+        var dfd = $.Deferred();
+        _.load.module({
+          key: 'editor-body',
+          url: "./templates/editor/index.mustache",
+          callback: function () {
+            _.print.module_only(
+              'editor-body',
+              data.body,
+              "article#article"
+            )(0, 0, 1);
+            dfd.resolve();
+          }
+        });
+        return dfd;
+      };
 
-            var documentLength = data.body.documents ? data.body.documents.length : 0;
-            var formLength = data.body.forms ? data.body.forms.length : 0;
-            if (documentLength < 8) {
-                data.body.empty_document = new Array(8 - documentLength);
-            }
-            if (formLength < 8) {
-                data.body.empty_form = new Array(8 - formLength);
-            }
+      var aside = function () {
+        var dfd = $.Deferred();
+        _.load.module({
+          key: 'editor-aside',
+          url: "./templates/editor/side_editor.mustache",
+          callback: function () {
+            _.print.module_only(
+              'editor-aside',
+              data.aside
+            )(30, 160, 2).addClass("pinned");
+            dfd.resolve();
+          }
+        });
+        return dfd;
+      };
 
-            _.load.module({
-                key: 'list-nav',
-                url: "./templates/list/nav.mustache",
-                callback: function () {
-                    _.print.module_only(
-                        'list-nav',
-                        data.nav,
-                        "nav#nav"
-                    )(0, 0);
-                }
-            });
-            _.load.module({
-                key: 'list-body',
-                url: "./templates/list/index.mustache",
-                callback: function () {
-                    _.print.module_only(
-                        'list-body',
-                        data.body,
-                        "article#article"
-                    )(0, 0);
-                }
-            });
-        }
-        ,
-        editor: function (data) {
-            data = data || {};
-            var nav = function () {
-                var dfd = $.Deferred();
-                _.load.module({
-                    key: 'editor-nav',
-                    url: "./templates/editor/nav.mustache",
-                    callback: function () {
-                        _.print.module_only(
-                            'editor-nav',
-                            data.nav,
-                            "nav#nav"
-                        )(0, 0, 1);
-                        dfd.resolve();
-                    }
-                });
-                return dfd;
-            };
+      var util = function () {
+        _.load.module('editor-util', "./templates/editor/util.mustache", function () {
+          _.print.module_only('editor-util');
+        });
+      };
 
-            var body = function () {
-                var dfd = $.Deferred();
-                _.load.module({
-                    key: 'editor-body',
-                    url: "./templates/editor/index.mustache",
-                    callback: function () {
-                        _.print.module_only(
-                            'editor-body',
-                            data.body,
-                            "article#article"
-                        )(0, 0, 1);
-                        dfd.resolve();
-                    }
-                });
-                return dfd;
-            };
+      nav().done(function () {
+        aside().done(function () {
+          body().done(function () {
+            util();
+          });
+        })
+      });
+    },
+    login: function (data) {
+      data = data || {};
+      var body = function () {
+        var dfd = $.Deferred();
+        _.load.module({
+          key: 'login-body',
+          url: "./templates/user/login.mustache",
+          callback: function () {
+            _.print.module_only(
+              'login-body',
+              data.body,
+              "article#article"
+            )(0, 0, 1);
+            dfd.resolve();
+          }
+        });
+        return dfd;
+      };
+      body();
+    },
+    signup: function (data) {
+      data = data || {};
+      var body = function () {
+        var dfd = $.Deferred();
+        _.load.module({
+          key: 'signup-body',
+          url: "./templates/user/signup.mustache",
+          callback: function () {
+            _.print.module_only(
+              'signup-body',
+              data.body,
+              "article#article"
+            )(0, 0, 1);
+            dfd.resolve();
+          }
+        });
+        return dfd;
+      };
+      body();
+    }
+  };
 
-            var aside = function () {
-                var dfd = $.Deferred();
-                _.load.module({
-                    key: 'editor-aside',
-                    url: "./templates/editor/side_editor.mustache",
-                    callback: function () {
-                        _.print.module_only(
-                            'editor-aside',
-                            data.aside
-                        )(90, 180, 2).addClass("pinned");
-                        dfd.resolve();
-                    }
-                });
-                return dfd;
-            };
+  $(document).on('click', 'a.inner', function (e) {
+    e.preventDefault();
+    router.do($(this).attr('href'), $(this).data('param'));
+  });
 
-            var util = function () {
-                _.load.module('editor-util', "./templates/editor/util.mustache", function () {
-                    _.print.module_only('editor-util');
-                });
-            };
-
-            nav().done(function () {
-                aside().done(function () {
-                    body().done(function () {
-                        util();
-                    });
-                })
-            });
-        },
-        login: function (data) {
-            data = data || {};
-            var body = function () {
-                var dfd = $.Deferred();
-                _.load.module({
-                    key: 'login-body',
-                    url: "./templates/user/login.mustache",
-                    callback: function () {
-                        _.print.module_only(
-                            'login-body',
-                            data.body,
-                            "article#article"
-                        )(0, 0, 1);
-                        dfd.resolve();
-                    }
-                });
-                return dfd;
-            };
-            body();
-        },
-        signup: function (data) {
-            data = data || {};
-            var body = function () {
-                var dfd = $.Deferred();
-                _.load.module({
-                    key: 'signup-body',
-                    url: "./templates/user/signup.mustache",
-                    callback: function () {
-                        _.print.module_only(
-                            'signup-body',
-                            data.body,
-                            "article#article"
-                        )(0, 0, 1);
-                        dfd.resolve();
-                    }
-                });
-                return dfd;
-            };
-            body();
-        }
-    };
+  return router;
 };
